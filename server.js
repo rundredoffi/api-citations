e120c2a012535a0f9d136c2ecd1edd18bbb719c0
Rundredoffi <contact.rundred@gmail.com>
2025-05-28 17:58:46 +0200

    feat: add express-rate-limit middleware to enhance rate limiting

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const fs = require('fs');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
+
+const globalLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 500, // 100 requêtes max par IP
+  standardHeaders: true, // Renvoie les headers RateLimit dans la réponse
+  legacyHeaders: false, // Désactive les X-RateLimit-* headers
+  message: {
+    status: 429,
+    error: "Trop de requêtes — merci de réessayer plus tard.",
+  },
+});
+
+app.use(globalLimiter);
 
 app.use(express.static('public'));
 
